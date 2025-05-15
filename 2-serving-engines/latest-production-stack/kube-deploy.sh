45a83e7cf8a18e94d2b6824f69aea5d4a0f9a91b
Samuel Shen <slshen@tensormesh.ai>
2025-05-15 04:52:35 +0000

    Figured out that that issue was with GKE A100 80GB Machines

--- 2-serving-engines/latest-production-stack/kube-deploy.sh
+++ 2-serving-engines/latest-production-stack/kube-deploy.sh
@@ -22,16 +22,7 @@ while true; do
   TOTAL=$(echo "$PODS" | tail -n +2 | wc -l)
   READY=$(echo "$PODS" | grep '1/1' | wc -l)
 
-  kubectl get pods -o name | grep deployment-vllm | while read pod; do
-    echo "Checking logs for $pod for CUDA OOM"
-    if kubectl logs $pod --tail=50 | grep "CUDA out of memory" >/dev/null; then
-      echo "❗ CUDA OOM detected in $pod"
-      exit 1
-    fi
-  done
-
-  # TODO: uncomment once Helm is deubugged!
-  Check for CrashLoopBackOff or other bad states
+  # Comment when debugging kubernetes
   if echo "$PODS" | grep -E 'CrashLoopBackOff|Error|ImagePullBackOff' > /dev/null; then
     echo "❌ Detected pod in CrashLoopBackOff / Error / ImagePullBackOff state!"
     kubectl get pods
@@ -39,6 +30,17 @@ while true; do
     kubectl delete all --all
     exit 1
   fi
+
+  kubectl get pods -o name | grep deployment-vllm | while read pod; do
+    echo "Checking logs for $pod for CUDA OOM"
+    if kubectl logs $pod --tail=50 | grep "CUDA out of memory" >/dev/null; then
+      echo "❗ CUDA OOM detected in $pod"
+      kubectl get pods
+      kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep '^vllm-' | xargs kubectl describe pod
+      kubectl delete all --all
+      exit 1
+    fi
+  done
 
   if [ "$READY" -eq "$TOTAL" ] && [ "$TOTAL" -gt 0 ]; then
     echo "✅ All $TOTAL pods are running and ready."
